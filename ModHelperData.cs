2c85039d082e9af89ff7e554ca8f567c35185fee
Myself <38400384+Myself-Bloons@users.noreply.github.com>
2025-12-16 11:43:09 +0100

    Update ModHelperData for version 1.1.0
    Updated version number and compatible version.

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -4,12 +4,13 @@ public static class ModHelperData
 {
     public const string Name = "Lives and Bananite Editor";
     public const string Description = "Click on your bananite or lives count in Frontiers Legends to edit them to any value you want.";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string RepoOwner = "Myself-Bloons";
     public const string RepoName = "LivesAndBananiteEditor";
     public const string Author = "Myself";
-    public const string WorksOnVersion = "52.1";
+    public const string WorksOnVersion = "52.2";
     public const string DllName = "LivesAndBananiteEditor.dll";
     public const string Icon = "Icon.png";
 }
+
 
